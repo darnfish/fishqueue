32c56bcf82d28e8ef99d426797a69dd5ad29cda7
William Gibson <william@tired.io>
2021-01-14 03:27:00 +0000

    Add equal port support to spammer

--- examples/spammer/index.js
+++ examples/spammer/index.js
@@ -1,11 +1,39 @@
 const axios = require('axios')
 const Listr = require('listr')
 
-const batchCount = 20
-const requestCount = 10
+const ports = ['4000', '5000', '6000', '7000']
 
-async function runSpamRequest(batch, request, { task }) {
-  const port = Math.random() >= 0.5 ? '4000' : '5000'
+const batchCount = 20
+const requestCount = 20
+
+let currentPortIndex = 0
+let currentPortBatch = 0
+
+function generateRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
+function generatePort() {
+  let port = ports[currentPortBatch]
+  if(!port) {
+    currentPortIndex = 0
+    currentPortBatch = 0
+
+    return generatePort()
+  }
+
+  const portBatchSize = requestCount / ports.length
+
+  currentPortIndex += 1
+  if(currentPortIndex >= portBatchSize) {
+    currentPortIndex = 0
+    currentPortBatch += 1
+  }
+
+  return port
+}
+
+async function runSpamRequest(batch, request, { port, task }) {
 
   const portLog = `port#${port}`
   const batchLog = `batch#${batch}`
@@ -30,7 +58,7 @@ async function main() {
     for(let request = 1; request <= requestCount; request++)
       requests.push({
         title: `Request #${request}`,
-        task: (ctx, task) => runSpamRequest(batch, request, { task })
+        task: (ctx, task) => runSpamRequest(batch, request, { task, port: generatePort() })
       })
 
     batches.push({
